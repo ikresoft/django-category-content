7563c4a924d77c24ac26afdf3229a2cf7fb702c6
Enver Bisevac <enver@Envers-MacBook-Pro.local>
2015-01-12 15:55:02 +0100

    removed warning for 1.8

--- category_content/forms.py
+++ category_content/forms.py
@@ -4,8 +4,10 @@ from models import CategoryContent
 from categories.models import Category
 from widgets import MpttTreeWidget
 
-class CategoryContentForm(ContentForm):
-	categories = forms.ModelMultipleChoiceField(required=True, queryset=Category.objects.all(), widget=MpttTreeWidget)
 
-	class Meta:
-		model = CategoryContent
+class CategoryContentForm(ContentForm):
+    categories = forms.ModelMultipleChoiceField(required=True, queryset=Category.objects.all(), widget=MpttTreeWidget)
+
+    class Meta:
+        model = CategoryContent
+        fields = "__all__"
